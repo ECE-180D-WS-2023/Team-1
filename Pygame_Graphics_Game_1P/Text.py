0e248db6cf46f3cdc4f33a7b1f11abd2f51c5698
Jonathan Lai <78984450+jonathan-c-lai@users.noreply.github.com>
2023-04-11 14:15:10 -0700

    Backup 1P completed into 1P

---
+++ Pygame_Graphics_Game_1P/Text.py
@@ -0,0 +1,10 @@
+import pygame
+from Settings import SCREEN_WIDTH
+
+class Text(pygame.sprite.Sprite):
+    def __init__(self, text = "", rect = (SCREEN_WIDTH - (SCREEN_WIDTH/5), 20)):
+        self.text = text
+        self.rect = rect
+    
+    def update(self, text):
+        self.text = text
